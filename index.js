f3875a61ab73d295bc1d824919beeb371a484f45
vnelai <118484488+vnelai@users.noreply.github.com>
2024-12-08 16:34:39 -0500

    Create user interaction with the API through a search feature

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-//Cache newImageButton into variable
-getNewImage = document.getElementById("newImageButton");
 
 //Add Event listener to newImageButton
+const getNewImage = document.getElementById("newImageButton");
 getNewImage.addEventListener("click", fetchCatPhotoApi);
 
 //Add functionality to callback function
-async function fetchCatPhotoApi () {
+async function fetchCatPhotoApi (input = "") {
     try {
+        //If user searches for a value then display images that match the value or else generate random picture 
+        let url;
+        if (input) {
+            url = `https://cataas.com/cat/says/${encodeURIComponent(input)}?position=center`;
+        } else {
+            url = "https://cataas.com/cat?position=center";
+        }
+
         //Get a cat image from API
-        const response = await fetch("https://cataas.com/cat?position=center");
+        const response = await fetch(url);
 
         //If network connection fails throw an error
         if (!response.ok) {
@@ -38,5 +45,22 @@ function displayImage (url) {
 
     //Append new image element to gallery-container
     gallery.appendChild(img);
+}
 
+//Add event listener for the search button
+const searchButton = document.getElementById("searchButton")
+const searchInput = document.getElementById('searchInput');;
+searchButton.addEventListener("click", searchCatPhotos);
+
+//Function for searching inputs
+async function searchCatPhotos(){
+    const input = searchInput.value.trim(); //Trim input from spaces
+    if (input) {
+        const imageUrl = await fetchCatPhotoApi(input); //pass search into API function to get relevant images
+        if (imageUrl) {
+            displayImage(imageUrl); //Display image that correlates to input
+        }
+    } else {
+        alert("Please enter a search input!"); //If user doesn't enter an input throw alert
+    }
 }
