27095eebe36daa1b87753baa4edfd7c152666622
vnelai <118484488+vnelai@users.noreply.github.com>
2024-12-08 15:24:51 -0500

    Create function for creating new img elements and displaying them on page

--- index.js
+++ index.js
@@ -24,4 +24,19 @@ async function fetchCatPhotoApi () {
     }
 }
 
+//Function for displaying images on page
+function displayImage (url) {
+    const gallery = document.getElementById("gallery");
 
+    //Clear previous images in gallery
+    gallery.innerHTML = "";
+
+    //Create new image element
+    const img = document.createElement("img");
+    img.src = url;
+    img.alt = "Random Cat Photo";
+
+    //Append new image element to gallery-container
+    gallery.appendChild(img);
+
+}
